10cc6bc0883466445c2ac3ebaddae266ded607d2
Jarek Chaninowicz <jarek.chaninowicz@razorbear.com>
2016-10-30 09:56:17 +0100

    Created Terrain Created Test "player" cube player moves on mouse click

---
+++ Assets/MouseClickMove.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseClickMove : MonoBehaviour {
+	Vector3 newPosition;
+	float duration = 5.0f;
+	float objectOriginalYAxis;
+	// Use this for initialization
+	void Start () {
+		objectOriginalYAxis = transform.position.y;
+		newPosition = transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown(0)) {
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray, out hit)) {
+				newPosition = hit.point;
+				newPosition.y = objectOriginalYAxis;
+			}
+		}
+
+		transform.position = Vector3.Lerp(transform.position, newPosition, 1 / (duration * (Vector3.Distance (transform.position, newPosition))));
+	}
+}
